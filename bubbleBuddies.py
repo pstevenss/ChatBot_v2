5a3ea209a5a84ad5eb7291a262faeddbda71f6c4
pstevens22 <stevensprada@gmail.com>
2023-05-25 17:51:18 -0400

    new character sprites v_2 , ability to jump and move around the screen without going off screen, fixed code indentation

--- bubbleBuddies.py
+++ bubbleBuddies.py
@@ -1,54 +1,80 @@
 import pygame as pg
+
 # pygame setup start --------------------
 pg.init()
-screen_width = 1000
-screen_height = 780
+screen_width = 928
+screen_height = 650
 screen = pg.display.set_mode((screen_width, screen_height))
+clock = pg.time.Clock()
+vel = 5
+x = 55
+y = 22
+width = 40
+height = 60
 pg.display.set_caption("Bubble Buddies")
 
-#load sprite characters
-forestBg = pg.image.load('sprite_Images/forestBg.png')
+# Load sprite characters
+forestBg = pg.image.load('sprite_Images/Background.png')
 forestBg = pg.transform.scale(forestBg, (screen_width, screen_height))
 
-sprite_sheet_image = pg.image.load('sprite_Images/characters.png').convert_alpha()
+adventIdle00 = pg.image.load('sprite_Images/adventurer-idle-00.png')
+adventIdle00 = pg.transform.scale(adventIdle00, (55, 42))
+adventIdle01 = pg.image.load('sprite_Images/adventurer-idle-01.png')
+adventIdle01 = pg.transform.scale(adventIdle01, (55, 42))
+adventIdle02 = pg.image.load('sprite_Images/adventurer-idle-02.png')
+adventIdle02 = pg.transform.scale(adventIdle02, (55, 42))
+adventIdle03 = pg.image.load('sprite_Images/adventurer-idle-03.png')
+adventIdle03 = pg.transform.scale(adventIdle03, (55, 42))
 
-black = (0,0,0)
-def getImage(sheet, frame, width, height, scale, color):
-    image = pg.Surface((width,height)).convert_alpha()
-    image.blit(sheet, (0,0), ((frame * width),0, width,height))
-    image = pg.transform.scale(image, (width * scale, height * scale))
-    image.set_colorkey(color)
-    return image
+isJump = False
+jumpCount = 5
 
-frame_0 = getImage(sprite_sheet_image, 0,  27, 27, 3, black)
-frame_1 = getImage(sprite_sheet_image, 1,  27, 27, 3, black)
-frame_2 = getImage(sprite_sheet_image, 2,  27, 27, 3, black)
-frame_3 = getImage(sprite_sheet_image, 3,  27, 27, 3, black)
+run = True
 
-#create animation list
-animate_list = []
-animate_steps = 4
+while run:
+    pg.time.delay(50)
 
-#for x in range(animate_steps):
+    # Background sprite Forest - dark
+    screen.blit(forestBg, (0, 0))
 
-
-running = True
-while running:
-    screen.blit(forestBg, (0,0))
-
-#show frame image
-    screen.blit(frame_0, (0,0))
-    screen.blit(frame_1, (500,0))
-    screen.blit(frame_2, (200,0))
-    screen.blit(frame_3, (300,0))
-
-
+    # Show sprite character images
+    screen.blit(adventIdle00, (0, 565))
+    screen.blit(adventIdle01, (25, 565))
+    screen.blit(adventIdle02, (50, 565))
+    screen.blit(adventIdle03, (75, 565))
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            running = False
+            run = False
+
+    keys = pg.key.get_pressed()
+
+    if keys[pg.K_LEFT] and x > vel:
+        x -= vel
+
+    if keys[pg.K_RIGHT] and x < screen_width - vel - width:
+        x += vel
+
+    if not isJump:
+        if keys[pg.K_UP] and y > vel:
+            y -= vel
+
+        if keys[pg.K_DOWN] and y < screen_height - height - vel:
+            y += vel
+
+        if keys[pg.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -5:
+            y -= (jumpCount * abs(jumpCount)) * 0.5
+            jumpCount -= 1
+        else:
+            jumpCount = 5
+            isJump = False
+
+    screen.fill((0, 0, 0))
     pg.display.update()
-    #
-    screen.fill("white")
+
 pg.quit()
+
 ## pygame setup end --------------------
