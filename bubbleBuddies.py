86ee874e0d65e1baf4309f52972b693c69776a96
pstevens22 <stevensprada@gmail.com>
2023-05-25 13:43:37 -0400

    added new sprite images for walking animation, played around with blitting adn resizing character sprites , and updated the variables for pygame BubbleBuddies folder

--- bubbleBuddies.py
+++ bubbleBuddies.py
@@ -1,16 +1,48 @@
 import pygame as pg
 # pygame setup start --------------------
 pg.init()
-window_width, window_height = 1250, 1000
-window = pg.display.set_mode((window_width, window_height))
+screen_width = 1280
+screen_height = 720
+screen = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_caption("Bubble Buddies")
+
+#load sprite characters
+#mainPlayer = pg.image.load('sprite_Images/mainPlayerIdleFront.png')
+mainPlayerSide1 = pg.image.load('sprite_Images/MainPlayerSpriteIdle1.png')
+mainPlayerSide2 = pg.image.load('sprite_Images/MainPlayerSpriteIdle2.png')
+mainPlayerLeftWalk = pg.image.load('sprite_Images/MainPlayerSpriteWalkLeft.png')
+mainPlayerRightWalk = pg.image.load('sprite_Images/MainPlayerSpriteWalkRight.png')
 
 running = True
 while running:
+    #screen.blit(mainPlayer, (100,100))
+    screen.blit(mainPlayerSide1, (500, 500))
+    screen.blit(mainPlayerLeftWalk, (400, 500))
+    screen.blit(mainPlayerRightWalk, (200, 500))
+    screen.blit(mainPlayerSide2, (300, 500))
+
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-    pg.display.flip()
-
+    pg.display.update()
+    #
+    screen.fill("white")
+    # pg.draw.circle(screen, "red", player_pos, 40)
+    #
+    # keys = pg.key.get_pressed()
+    # if keys[pg.K_w]:
+    #     player_pos.y -= 300 * dt
+    # if keys[pg.K_s]:
+    #     player_pos.y += 300 * dt
+    # if keys[pg.K_a]:
+    #     player_pos.x -= 300 * dt
+    # if keys[pg.K_d]:
+    #     player_pos.x += 300 * dt
+    #
+    # dt = clock.tick(60) / 1000
+    #
+    #
+    #
 pg.quit()
 ## pygame setup end --------------------
