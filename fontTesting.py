a6806c6aeca3089bf813eb8edfce6fe9481a8a76
pstevens22 <stevensprada@gmail.com>
2023-05-27 17:21:43 -0400

    added font folder, more fonts to come but fontTesting file is finished and can be used as future reference

--- fontTesting.py
+++ fontTesting.py
@@ -2,15 +2,16 @@ import pygame as pg
 pg.init()
 clock = pg.time.Clock()
 fps = 60
-font = pg.font.Font('Romulus.ttf', 52) #choosing text pack
+font = pg.font.Font('fonts/Romulus.ttf', 52) #choosing text pack and the font size
+# add multiple font packs and call it in while loop
 screen = pg.display.set_mode([800,500])
 
 run = True
 while run:
     clock.tick(fps) #references fps variable for game speed
     screen.fill('orange') #bg color
-    my_text = font.render('check out this text', True, 'white') #setting text , making it true, and choosing the color
-    screen.blit(my_text, (100,150)) #blitting text onto window
+    my_text = font.render('check out this text', False , 'white') #setting text , making it false for crisper look rather than true, and choosing the color
+    screen.blit(my_text, (100,150)) #blitting text onto window and putting the position on the window
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
